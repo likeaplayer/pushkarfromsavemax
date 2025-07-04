887e3e1d9a867f9d4b909eac2ac578d91f398382
Pushkar Sindhu <pushkarsindhu@live.com>
2025-07-04 17:06:12 -0400

    Add favicon for live site

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+// pages/_app.js
 import '../styles/globals.css'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* Or if you use png */}
+        {/* <link rel="icon" type="image/png" href="/favicon.png" /> */}
+        <title>Pushkar Sindhu</title> {/* You can set page title here too */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
