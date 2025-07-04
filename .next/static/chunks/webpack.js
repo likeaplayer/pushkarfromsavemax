50962800ebdc11c54bdee0a2f9c5af7f5af59137
Pushkar Sindhu <pushkarsindhu@live.com>
2025-07-04 17:33:30 -0400

    Updated background image and design

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0d16f92b72e65d04"; }
+/******/ 		__webpack_require__.h = function() { return "5a32d4a36639446f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
